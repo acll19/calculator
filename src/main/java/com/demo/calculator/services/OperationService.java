3efdcde2e5f3b103ce160723bc522ee6135b1b33
jaironegrete12 <jairo_negrete@hotmail.com>
2018-11-23 08:09:32 -0500

    add operations

--- src/main/java/com/demo/calculator/services/OperationService.java
+++ src/main/java/com/demo/calculator/services/OperationService.java
@@ -14,10 +14,24 @@ public class OperationService implements OperationInterface {
         return add ? (a + b) : (a - b);
     }
 
+    private Long multiplication(Long a, Long b) {
+        return (a * b);
+    }
+
+    private Long divide(Long a, Long b){
+        return (a / b);
+    }
+
     @Override
     public Long calculate(CalculatorForm calculatorForm) {
         if(OperationEnum.add.getOperationCode() == calculatorForm.getOperation()){
             return add(calculatorForm.getA(), calculatorForm.getB(), true);
+        } else if (OperationEnum.substrac.getOperationCode() == calculatorForm.getOperation()){
+            return add(calculatorForm.getA(), calculatorForm.getB(), false);
+        } else if (OperationEnum.multiplication.getOperationCode() == calculatorForm.getOperation()){
+            return multiplication(calculatorForm.getA(), calculatorForm.getB());
+        } else if (OperationEnum.division.getOperationCode() == calculatorForm.getOperation()){
+            return divide(calculatorForm.getA(), calculatorForm.getB());
         }
         return 0L;
     }
